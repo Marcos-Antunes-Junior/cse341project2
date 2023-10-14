707a90f8639775349b98c2c65607214270e37768
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-10-14 11:00:05 -0300

    "fixed swagger.json file"

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,7 +1,7 @@
 const baseController = {}
 
 baseController.buildHome = async function (req, res) {
-res.send('Hello! This is the employee API.');
+res.send('Hello! This is the employee API. Access the API documentation: https://cse341project2-bkgg.onrender.com/api-docs/');
 }
 
 module.exports = baseController;
